82381d09e2ab95c36c63a6793d46c36abe73b123
Entrecasteaux <145047309+Entrecasteaux@users.noreply.github.com>
2022-12-04 09:00:12 +0000

    gpio: unregister gpiochip device before removing it

--- internal/openai/openai.go
+++ internal/openai/openai.go
@@ -20,4 +20,5 @@ type CompletionRequest struct {
 	Suffix           string  `json:"suffix,omitempty"`
 	MaxTokens        int     `json:"max_tokens,omitempty"`
 	Temperature      float64 `json:"temperature,omitempty"`
-	TopP             float64 `json:"top_p,omitempt
+	TopP             float64 `json:"top_p,omitempty"`
+	N                int     `j
