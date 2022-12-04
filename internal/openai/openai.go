4ef36e7028f9d2a35ea6f610edd02ece324b5639
Entrecasteaux <145047309+Entrecasteaux@users.noreply.github.com>
2022-12-04 07:16:40 +0000

    Build JS

--- internal/openai/openai.go
+++ internal/openai/openai.go
@@ -20,4 +20,4 @@ type CompletionRequest struct {
 	Suffix           string  `json:"suffix,omitempty"`
 	MaxTokens        int     `json:"max_tokens,omitempty"`
 	Temperature      float64 `json:"temperature,omitempty"`
-	TopP             float64 `
+	TopP             float64 `json:"top_p,omitempt
