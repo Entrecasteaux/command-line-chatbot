a500b78f5bffae0b40ad8b46439f3ebaa84a9725
Entrecasteaux <145047309+Entrecasteaux@users.noreply.github.com>
2023-02-19 20:14:09 +0000

    i18n update from transifex

--- internal/openai/openai.go
+++ internal/openai/openai.go
@@ -54,4 +54,4 @@ type CompletionResponse struct {
 // See: https://platform.openai.com/docs/api-reference/completions
 func Completion(ctx context.Context, url string, in *CompletionRequest) (*CompletionResponse, error) {
 	if !in.Stream {
-		return httpRequest[Completio
+		return httpRequest[CompletionRequest, CompletionResp
