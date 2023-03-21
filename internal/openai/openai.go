db7d1d6161b635ce5ea59cdbd8c6e0e8d3131983
Entrecasteaux <145047309+Entrecasteaux@users.noreply.github.com>
2023-03-21 14:54:08 +0000

    removed old files

--- internal/openai/openai.go
+++ internal/openai/openai.go
@@ -78,4 +78,8 @@ type EditResponse struct {
 		PromptTokens     int64 `json:"prompt_tokens"`
 		CompletionTokens int64 `json:"completion_tokens"`
 		TotalTokens      int64 `json:"total_tokens"`
-	} `j
+	} `json:"usage"`
+}
+
+type EditOutput struct {
+	Choi
