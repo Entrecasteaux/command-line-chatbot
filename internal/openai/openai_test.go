f8f848508c640d3de9d4ac8cd8e1a027828d2d17
Entrecasteaux <145047309+Entrecasteaux@users.noreply.github.com>
2023-06-25 10:14:13 +0000

    [IMJ][imj3][processing] Updated VisualAnalysis.

--- internal/openai/openai_test.go
+++ internal/openai/openai_test.go
@@ -2,3 +2,31 @@
 // Copyright 2023 Changkun Ou. All rights reserved.
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
+
+package openai_test
+
+import (
+	"context"
+	"os"
+	"strings"
+	"testing"
+	"unicode/utf8"
+
+	"changkun.de/x/chat/internal/openai"
+)
+
+func countWords(s string) int {
+	counter := 0
+	for _, word := range strings.Fields(s) {
+		runeCount := utf8.RuneCountInString(word)
+		if len(word) == runeCount {
+			counter++
+		} else {
+			counter += runeCount
+		}
+	}
+
+	return counter
+}
+
+func TestEdit(t *testing.T) {
