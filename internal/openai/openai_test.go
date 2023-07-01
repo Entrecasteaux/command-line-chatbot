9d894f39d8f5509c0fb5fbb8633ab4c127a7c3f1
Entrecasteaux <145047309+Entrecasteaux@users.noreply.github.com>
2023-07-01 20:23:13 +0000

    Edit README.md

--- internal/openai/openai_test.go
+++ internal/openai/openai_test.go
@@ -30,3 +30,32 @@ func countWords(s string) int {
 }
 
 func TestEdit(t *testing.T) {
+	txt, err := os.ReadFile("testdata/data.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("total words: ", countWords(string(txt)))
+
+	// TLDR summarization: https://beta.openai.com/examples/default-tldr-summary
+	o, err := openai.Edit(context.Background(), &openai.EditRequest{
+		Model:       "text-davinci-edit-001",
+		Input:       string(txt),
+		Instruction: "Fix the grammar and spelling mistakes.",
+		Temperature: 0.5,
+		N:           1,
+	})
+	if err != nil {
+		t.Fatalf("failed to edit: %v", err)
+	}
+
+	for _, c := range o.Choices {
+		t.Log(c.Text)
+		t.Log("words: ", countWords(c.Text))
+	}
+}
+
+func TestChat(t *testing.T) {
+	respCh, errCh := openai.Chat(context.Background(), &openai.ChatRequest{
+		Model:  "gpt-4",
+		Stream: false,
+		Message: []openai.ChatMessage{
