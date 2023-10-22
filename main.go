5979b475076f513659aebb5a920aa8eb9facfed2
Entrecasteaux <145047309+Entrecasteaux@users.noreply.github.com>
2023-10-22 22:25:37 +0000

    Apply suggestions from code review

--- main.go
+++ main.go
@@ -6,3 +6,31 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"changkun.de/x/chat/internal/openai"
+	"changkun.de/x/chat/internal/term"
+)
+
+func main() {
+	if os.Getenv("OPENAI_API_KEY") == "" {
+		fmt.Fprint(os.Stderr, "Please set OPENAI_API_KEY environment variable.\n")
+		return
+	}
+
+	stdin := os.Stdin
+	stdout := os.Stdout
+	fmt.Fprint(stdout, term.Orange("Hi, I'm a chatbot. How can I help you?\n"))
+
+	session := []openai.ChatMessage{
+		{
+			Role:    "system",
+			Content: "You are a helpful assistant.",
+		},
+	}
+
+	for {
